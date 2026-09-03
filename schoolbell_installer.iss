78a3ce0bd53dbb7a5d2bce453968e20a26678c18
Andrii Bolshakov <12asic12@gmail.com>
2026-09-03 08:43:21 +0300

    Release v1.2.3

--- schoolbell_installer.iss
+++ schoolbell_installer.iss
@@ -1,6 +1,6 @@
 ; Inno Setup Script for SchoolBell
 #define AppName "SchoolBell"
-#define AppVersion "1.2.2"
+#define AppVersion "1.2.3"
 #define AppPublisher "SchoolBell Team"
 #define AppExeName "SchoolBell.exe"
 #define DistDir "dist\SchoolBell"
--- src/main/java/com/schoolbell/MainApp.java
+++ src/main/java/com/schoolbell/MainApp.java
@@ -38,7 +38,7 @@ import static com.schoolbell.ui.UIStyles.*;
 
 public class MainApp extends Application {
     private static final Logger logger = LoggerFactory.getLogger(MainApp.class);
-    public static final String VERSION = "1.2.2";
+    public static final String VERSION = "1.2.3";
     private static final String APP_TITLE = "SchoolBell v" + VERSION;
 
     // Services
